a0de9a67e83de2b363c5387676f42d5011386a50
username <username@example.com>
2026-05-27 17:35:16 +0530

    fix: server.js

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ import app from 'app.js';
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, async (req, res) => {
-    console.log("server is up and running on PORT ", PORT); 
+    console.log(`server is up on running on ${PORT}`); 
 } )
