69efc222666681488692d267deb307072ce63459
username <username@example.com>
2026-05-27 18:04:17 +0530

    remote branch delete and create is done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors'; 
 
+import authRoute from './routes/auth.routes.js';
 
 
 const app = express(); 
 
 app.use(cors()); 
 app.use(express.json()); 
+
+
+app.use('/api/auth', authRoute); 
 
 app.use('/', (req, res)=> {
     console.log("application level route"); 
